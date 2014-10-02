533e022ef9e7b623a94dd95d110026a77a60787c
joke2k <joke2k@gmail.com>
2014-10-02 19:46:15 +0200

    add site.name to logo

--- project/accesso/contexts.py
+++ project/accesso/contexts.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 
 def project_context(request):
     return {
-        'project_name': settings.PROJECT_NAME
+        'project_name': settings.PROJECT_NAME,
+        'site': Site.objects.get_current(),
     }
